0e5fd1022521f7e227e846c8c5715385f7b9f4a6
Vladimir Litvinov <litvinovvo@gmail.com>
2020-01-16 22:38:17 +0200

    fix import urls

--- experiments/network-test/js/entry.js
+++ experiments/network-test/js/entry.js
@@ -1,5 +1,5 @@
-import 'heavy-es6.js';
-import 'heavy1-es6.js';
-import 'heavy2-es6.js';
-import 'heavy3-es6.js';
-import 'heavy4-es6.js';
+import './heavy-es6.js';
+import './heavy1-es6.js';
+import './heavy2-es6.js';
+import './heavy3-es6.js';
+import './heavy4-es6.js';
