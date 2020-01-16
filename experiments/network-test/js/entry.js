46feea9bda19116131d009bc2028de97ceac2315
Vladimir Litvinov <litvinovvo@gmail.com>
2020-01-17 00:16:36 +0200

    dynamic import for es6

--- experiments/network-test/js/entry.js
+++ experiments/network-test/js/entry.js
@@ -1,7 +1,19 @@
-import './heavy-es6.js';
-import './heavy1-es6.js';
-import './heavy2-es6.js';
-import './heavy3-es6.js';
-import './heavy4-es6.js';
+// import './heavy-es6.js';
+// import './heavy1-es6.js';
+// import './heavy2-es6.js';
+// import './heavy3-es6.js';
+// import './heavy4-es6.js';
+
+// console.log('hello from entry 1');
 
 console.log('hello from entry 1');
+
+window.pageStructure.modules.forEach(moduleConfig => {
+    module.import(`./${moduleConfig.name}-es6.js`)
+        .then(module => {
+            console.log('module', module);
+        })
+        .catch(error => {
+            console.log('error', error);
+        });
+});
