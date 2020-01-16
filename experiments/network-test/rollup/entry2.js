da6b46df1a7bda2cf9d8cdbc715574003258128a
Vladimir Litvinov <litvinovvo@gmail.com>
2020-01-16 23:07:58 +0200

    system with many entries

---
+++ experiments/network-test/rollup/entry2.js
@@ -0,0 +1,11 @@
+System.register(['./heavy-es6.js', './heavy1-es6.js', './heavy2-es6.js', './heavy3-es6.js', './heavy4-es6.js'], function () {
+	'use strict';
+	return {
+		setters: [function () {}, function () {}, function () {}, function () {}, function () {}],
+		execute: function () {
+
+			console.log('hello from entry 2');
+
+		}
+	};
+});
